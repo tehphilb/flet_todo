3024f98a13f75bb353d772b27a1fb7d055d3e840
tehphilb <tehphilb@gmail.com>
2023-01-19 23:53:33 +0100

    functional

--- app_2.py
+++ app_2.py
@@ -1,4 +1,4 @@
-
+# https: // www.youtube.com/watch?v = qRqxAUC_4oA & t = 3500s
 from flet import *
 from datetime import datetime
 import sqlite3
@@ -7,8 +7,8 @@ import sqlite3
 
 
 class FormContainer(UserControl):
-    def __init__(self):
-        # self.func = func
+    def __init__(self, func):
+        self.func = func
         super().__init__()
 
     def build(self):
@@ -18,7 +18,7 @@ class FormContainer(UserControl):
             bgcolor="#f0f0f0",
             opacity=0,
             border_radius=0,
-            padding=padding.only(top=45, bottom=45),
+            padding=padding.only(top=25, bottom=35),
             animate=animation.Animation(500, "decelerate"),
             animate_opacity=280,
             content=Column(
@@ -31,34 +31,120 @@ class FormContainer(UserControl):
                     border_color="black87",
                     border_width=0.3,
                     hint_text="Write something...",
-                    hint_style=TextStyle(color="black87", weight=FontWeight.W_200),
-                    text_style=TextStyle(color="black87", weight=FontWeight.W_300),
+                    hint_style=TextStyle(
+                        color="black87", weight=FontWeight.W_200),
+                    text_style=TextStyle(
+                        color="black87", weight=FontWeight.W_300),
                     cursor_width=0.4,
                     multiline=True,
                     min_lines=1,
                     max_lines=2,
                 ),
-                IconButton(
-                    content=Text("Add TODO"),
-                    width=110,
-                    style=ButtonStyle(
-                        bgcolor="black87",
-                        shape=StadiumBorder(),
-                    ),
-                )
-                ]
+                    Container(height=5),                   
+                    IconButton(
+                        icon=icons.ADD,
+                        icon_size=32,
+                        icon_color="black87",
+                    # content=Text("Add TODO"),
+                    # width=110,
+                    # style=ButtonStyle(
+                    #     bgcolor="black87",
+                    #     shape=StadiumBorder(),
+                    # ),
+                    on_click=self.func,
+                )]
             )
         )
+
+# create todo class
+
+
+class CreateTodo(UserControl):
+    def __init__(self, todo: str, date: str):
+        self.todo = todo
+        self.date = date
+        super().__init__()
+
+    def deleteEditTodo(self, name, color):
+        return IconButton(
+            icon=name,
+            icon_size=18,
+            icon_color=color,
+            animate_opacity=280,
+            on_click=None,
+        )
+
+    def build(self):
+        return Row(
+            alignment=MainAxisAlignment.CENTER,
+            controls=[
+                Container(
+                    width=260,
+                    height=60,
+                    border=border.all(0.3, "white"),
+                    border_radius=5,
+                    # on_hover=None,
+                    clip_behavior=ClipBehavior.ANTI_ALIAS,
+                    padding=10.0,
+                    content=Row(
+                        alignment=MainAxisAlignment.SPACE_BETWEEN,
+                        controls=[
+                            Column(
+                                spacing=1,
+                                alignment=MainAxisAlignment.CENTER,
+                                controls=[
+                                    Text(value=self.todo, size=12),
+                                    Text(value=self.date, size=8,
+                                         color="white54"),
+                                ]
+                            ),
+                            ##TODO: replace below with dropdown and 3 dot menu
+                            # Icons delete and Exit
+                            Row(
+                                spacing=1,
+                                alignment=MainAxisAlignment.SPACE_EVENLY,
+                                controls=[
+                                    # Calls deleteTodo function
+                                    self.deleteEditTodo(icons.DELETE, "red"),
+                                    self.deleteEditTodo(icons.EDIT, "green"),
+                                ],
+                            )
+                        ]
+                    )
+
+                )])
 
 
 def main(page: Page):
     page.horizontal_alignment = 'center'
     page.vertical_alignment = 'center'
 
+    # datetime function
+    def dateTimeFunc():
+        return datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+
+    # create tot input screen
+
+    def createTodoScreen(e):
+        dateTime = dateTimeFunc().split(" ")[0]
+
+        # use form variable to get data from tesxtfield
+        if form.content.controls[0].value:  # checks textflied value
+            _main_column_.controls.append(
+                # create an instance of CreateTodo class
+                CreateTodo(
+                    # todo takes two arguments
+                    form.content.controls[0].value,
+                    dateTime,
+                )
+            )
+            _main_column_.update()
+
+            createTodo(e)
+
     # funtion to show/hide form container
     def createTodo(e):
         if form.height != 200:
-            
             form.height, form.opacity, form.border_radius = 200, 1, 20
             form.update()
         else:
@@ -90,11 +176,11 @@ def main(page: Page):
                                on_click=createTodo,)
                 ],
             ),),
-            Container(
-                alignment=alignment.center,
-                padding=padding.only(top=35, left=10, right=10),
-                content=Divider(height=1, color="#f0f0f0"),
-            ),
+            # Container(
+            #     alignment=alignment.center,
+            #     padding=padding.only(top=35, left=10, right=10),
+            #     content=Divider(height=1, color="#f0f0f0"),
+            # ),
         ],
     )
 
@@ -108,14 +194,14 @@ def main(page: Page):
             border_radius=40,
             border=border.all(0.5, "white"),
             padding=padding.only(top=100),
-            clip_behavior=ClipBehavior.HARD_EDGE,
+            clip_behavior=ClipBehavior.ANTI_ALIAS,
             content=Column(
                 alignment=MainAxisAlignment.CENTER,
                 controls=[
                     # main column here
                     _main_column_,
                     # form class here
-                    FormContainer(),
+                    FormContainer(lambda e: createTodoScreen(e)),
                 ]
             )
         )
@@ -127,6 +213,8 @@ def main(page: Page):
     # now it can be called from wherever in the code faster and easier
     # route is page.add.Container => Column.FormContainer => Container
     form = page.controls[0].content.controls[1].controls[0]
+    print(form.content.controls[0].value)
+
 
 if __name__ == "__main__":
     app(target=main)
